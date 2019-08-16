4c9c59a3896426466648ee9a2a6ec5c0bdb4731e
Michelle Harrop <harrop.michelle@gmail.com>
2019-08-16 13:08:45 -0400

    added something to register

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -11,4 +11,24 @@ $(function ()
     {
         object = data;
     });
+
+    $("#addMe").on("click", function()
+    {
+        sendContact();
+        $("#addMe").prop("href", "details.html?courseId=" + object.CourseId) 
+    });
+    
+
+
+
+    $("#cancel").prop("href", "details.html?courseId=" + object.CourseId)
 });
+
+function sendContact()
+{
+    $.post("/api/register/", $("#registerForm").serialize(), function(data)
+    {
+        $("#msgDiv").html(data)
+    });
+    return false;
+}
