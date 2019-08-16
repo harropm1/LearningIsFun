55f498e5aacdaeee3985b07d3f689e4be60002c3
Michelle Harrop <harrop.michelle@gmail.com>
2019-08-16 13:40:38 -0400

    I made too many changes to go back

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -6,29 +6,16 @@ $(function ()
     let courseid = urlParams.get("courseId");
     $("#courseid").val(courseid);
 
-    let object;
-    $.getJSON("/api/register/" + courseid, function(data)
-    {
-        object = data;
-    });
+    $("#addMe").on("click", sendContact);
 
-    $("#addMe").on("click", function()
-    {
-        sendContact();
-        $("#addMe").prop("href", "details.html?courseId=" + object.CourseId) 
-    });
-    
-
-
-
-    $("#cancel").prop("href", "details.html?courseId=" + object.CourseId)
+    /* $("#cancel").prop("href", "details.html?courseId=" + object.courseid) */
 });
 
 function sendContact()
 {
-    $.post("/api/register/", $("#registerForm").serialize(), function(data)
+    $.post("/api/register", $("#registerForm").serialize(), function(data)
     {
-        $("#msgDiv").html(data)
+        $("#msgDiv").html("See you soon!");
     });
     return false;
 }
